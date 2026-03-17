0323bdb6b7be99461081a859de4cbcf480624369
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:42:07 -0300

    Implement area and perimeter calculation for rectangle
    Added functionality to calculate area and perimeter of a rectangle.

--- input_calc.py
+++ input_calc.py
@@ -3,3 +3,10 @@ def rectangle():
     el área y el perímetro.
     """
     pass
+
+    base = int(input("ingrese base"))
+    altura = int(input("ingrese altura"))
+    area_rectangulo = base * altura
+    print (area_rectangulo)
+    perimetro_rectangulo = base + altura
+    print (perimetro_rectangulo)
