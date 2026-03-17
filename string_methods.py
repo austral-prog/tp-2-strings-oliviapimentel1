567f14dbad8085a54ad5d0fafbe0b84f55bf35f3
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:44:26 -0300

    Add string manipulation methods in string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -8,3 +8,28 @@ def string_methods():
     multilinea = """Linea 1
     Linea 2
     Linea 3"""
+
+    print (nombre.strip())
+    print (nombre.lstrip())
+    print (nombre.rstrip())
+
+    print (frase.lower())
+    print (frase.upper())
+    print (frase.title())
+
+    print (frase.find("gran"))
+
+    print (frase.replace("programacion","desarrollo"))
+
+    print (frase.count("a"))
+
+    print ("Python" in frase)
+    print ("Java" in frase)
+
+    print (frase[0:7])
+    print (frase[0:7:2])
+    print (frase[0:7] [::-1])
+
+    print (f"{nombre.strip()} + python")
+
+    print (multilinea)
