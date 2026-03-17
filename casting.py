c95ab9ee8f35ec059cb32e3c41a0913f791e61a3
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:39:50 -0300

    Add price calculation function with discount input
    Implement price calculation with discount and quantity.

--- casting.py
+++ casting.py
@@ -1,3 +1,10 @@
 def casting():
     """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
     pass
+
+    precio = float(input("ingrese el precio:"))
+    descuento = int(input("ingrese el descuento:"))
+    cantidad = float(input("ingrese la cantidad:"))
+
+    precio_con_descuento = ((precio - descuento) * cantidad)
+    print (precio_con_descuento)
