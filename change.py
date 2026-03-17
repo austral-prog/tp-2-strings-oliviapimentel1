37a1eee9803ea60538e802db859de5949ac4e139
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:40:15 -0300

    Implement change calculation in change.py

--- change.py
+++ change.py
@@ -3,3 +3,16 @@ def change():
     y lo separa en pesos (parte entera) y centavos.
     """
     pass
+
+    gasto = float(input("ingrese su gasto\n"))
+    print (gasto)
+
+    dinero = int(input("ingrese su dinero\n"))
+    print (dinero)
+
+    vuelto = dinero - gasto
+
+    pesos = int(vuelto)
+    print (pesos)
+    centavos = round(vuelto - pesos) * 100
+    print (centavos)
