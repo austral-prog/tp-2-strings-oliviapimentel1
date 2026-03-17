b1e519b1805a16b583c3e963fbcd8f09fab47b0b
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:41:22 -0300

    Add vowel check functionality to in_string.py

--- in_string.py
+++ in_string.py
@@ -3,3 +3,9 @@ def check_vowels():
     sin distinguir mayúsculas de minúsculas.
     """
     pass
+    nombre = input("ingrese su nombre").lower()
+    print (f"Contiene a: {"a" in nombre}")
+    print(f"Contiene e: {"e" in nombre}")
+    print(f"Contiene i: {"i" in nombre}")
+    print(f"Contiene o: {"o" in nombre}")
+    print(f"Contiene u: {"u" in nombre}")
