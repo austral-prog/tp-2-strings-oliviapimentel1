f771769ccf1ecce6496826cee493375354b8edcd
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:43:56 -0300

    Add functionality to display string information

--- string_info.py
+++ string_info.py
@@ -3,3 +3,10 @@ def string_info():
     la palabra repetida 3 veces y decorada con '***'.
     """
     palabra = "Programacion"
+
+    print (palabra)
+    print (len(palabra))
+    print (palabra [0])
+    print (palabra [-1])
+    print (palabra * 3)
+    print ("***" + palabra + "***")
