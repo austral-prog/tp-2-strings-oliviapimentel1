8358d5b9d3c7b3b6a97f54452ca3e9f1b7063686
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-17 20:42:30 -0300

    Add functionality to input and format full name

--- names.py
+++ names.py
@@ -3,3 +3,9 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
+    nombre = input("escriba su nombre")
+    apellido = input("escriba su apellido")
+    nombre_completo = nombre + apellido
+    print (nombre_completo.lower())
+    print (nombre_completo.upper())
+    print (nombre_completo.title())
